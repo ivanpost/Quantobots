a4e6717ca0098ab2d380a729e7cb2bda0db837a2
ivanpost <postnikov.shiri@gmail.com>
2026-04-04 13:55:10 +0400

    Считывание 6 датчиков

---
+++ Line6SensorRead.cpp
@@ -0,0 +1,51 @@
+/*
+Параметры блока: <Sn1.DI> ... <Sn6.DI> - входы платы
+Параметры вылета: <exitForce6.Name> (рекомендуемое 7.0) переменная - форсирование при вылете
+Вход: <invertSens>
+Выходы: currentError, sensorMask -> rawSensor
+*/
+
+const int sensorPins[] = {<Sn1.DI>, <Sn2.DI>, <Sn3.DI>, <Sn4.DI>, <Sn5.DI>, <Sn6.DI>};
+// Веса распределены симметрично: -5, -3, -1, 1, 3, 5
+const float weights[] = {-5.0, -3.0, -1.0, 1.0, 3.0, 5.0}; 
+
+int sensorMask = 0; 
+float currentError = 0;
+float lastError = 0;
+
+void loop() {
+  currentError = calculateError(); 
+}
+
+float calculateError() {
+    float sumWeights = 0;
+    int activeSensors = 0;
+    int currentMask = 0;
+ 
+    for (int i = 0; i < 6; i++) {
+      int rawVal = digitalRead(sensorPins[i]);
+      // Инверсия: если invertSens=false, считаем активным LOW (0), преобразуя в 1
+      int val = (<invertSens>) ? rawVal : !rawVal; 
+ 
+      if (val == 1) {
+         sumWeights += weights[i];
+         activeSensors++;
+         // Формируем 6-битную маску (S1 - старший бит)
+         currentMask |= (1 << (5 - i));
+      }
+    }
+    sensorMask = currentMask;
+ 
+    if (activeSensors > 0) {
+      // Средневзвешенная ошибка
+      float error = sumWeights / (float)activeSensors;
+      lastError = error;
+      return error;
+    } else {
+      // Логика вылета: если последняя ошибка была сильно левее/правее центра
+      float eForceM = constrain((<exitForce6.Name>), 6.0, 15.0);
+      if (lastError < -1.0) return -eForceM; 
+      if (lastError > 1.0) return eForceM;
+      return 0;
+    }
+}
